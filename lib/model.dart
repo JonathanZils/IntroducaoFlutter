fc84667bbc200291707e4596d46fba0ec322a12e
Maria Clara <mcfranca0107@gmail.com>
2022-05-02 15:13:52 -0300

    exercicio 4

--- lib/model.dart
+++ lib/model.dart
@@ -6,24 +6,45 @@ class Pessoa{
   DateTime? nascimento;
   int? idade;
   Sexo? sexo;
-  double? salario;
-  bool? programador;
+
+int? getIdade(){
+  if(nascimento == null){
+    return null;
+  }
+  final hoje = DateTime.now();
+  final idade = hoje.difference(nascimento!);
+  return idade.inDays ~/ 365;
+
+}
 
   @override
   String toString(){
-    return "{nome=$nome, cpf=$cpf, nacimento=$nascimento, idade=$idade, "
-        "sexo=$sexo, salario=$salario, programador=$programador}";
+    return "{nome=$nome, cpf=$cpf, nacimento=$nascimento, idade=$getIdade(), "
+        "sexo=$sexo";
+  }
+}
+class Programador extends Pessoa{
+
+  double? salario;
+
+
+  String toString(){
+    return "{nome=$nome, cpf=$cpf, nacimento=$nascimento, idade=$getIdade(), "
+        "sexo=$sexo, salario=$salario";
   }
 }
 
+
 void mainEntidades(){
-  Pessoa pessoa = new Pessoa();
+  Pessoa pessoa = Programador();
+
   pessoa.nome = "zils";
   pessoa.cpf = "089.124.214-50";
   pessoa.nascimento = DateTime(1995,9,12);
   pessoa.idade = 26;
   pessoa.sexo = Sexo.masculino;
-  pessoa.salario = 2.500;
-  pessoa.programador = true;
-  print(pessoa);
+  //pessoa.salario = 2.500;
+
+  print(pessoa.toString);
 }
+
