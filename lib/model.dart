79d1da0644ad3d17a3147bab7d532102dc9d054d
Maria Clara <mcfranca0107@gmail.com>
2022-05-02 17:04:04 -0300

    avanços no exercicio

--- lib/model.dart
+++ lib/model.dart
@@ -1,11 +1,41 @@
-enum Sexo{ masculino, feminino}
+import 'package:flutter/cupertino.dart';
+
+enum Sexo{ MASCULINO, FEMININO}
+
+Sexo? strToSexo(String? str){
+  if(str == null){
+    return null;
+  }
+  switch(str.toUpperCase()){
+    case'MASCULINO':
+    case'M':
+      return Sexo.MASCULINO;
+    case'FEMININO':
+    case'F':
+      return Sexo.FEMININO;
+    default:
+      return null;
+  }
+}
+
+
 
 class Pessoa{
+
+
   String? nome;
   String? cpf;
   DateTime? nascimento;
   int? idade;
   Sexo? sexo;
+
+  Pessoa(this.nome, this.cpf, this.nascimento, this.sexo);
+  Pessoa.fromMap(Map<String ,String> map){
+    nome = map['nome'];
+    cpf = map['cpf'];
+    nascimento = DateTime.tryParse(map['nascimento'] ?? '2001-01-01');
+    sexo = strToSexo(map['sexo']);
+  }
 
 int? getIdade(){
   if(nascimento == null){
@@ -26,7 +56,12 @@ int? getIdade(){
 class Programador extends Pessoa{
 
   double? salario;
+  Programador(String? nome, String? cpf, DateTime? nascimento, Sexo? sexo, this.salario)
+  : super(nome, cpf, nascimento, sexo);
 
+  Programador.fromMap(Map<String, String> map) : super.fromMap(map){
+    salario = double.tryParse(map['salario'] ?? "0.0");
+  }
 
   String toString(){
     return "{nome=$nome, cpf=$cpf, nacimento=$nascimento, idade=$getIdade(), "
@@ -36,15 +71,26 @@ class Programador extends Pessoa{
 
 
 void mainEntidades(){
-  Pessoa pessoa = Programador();
+  final map = {
+    'nome': 'Zils',
+    'nascimento': '1995-09-12',
+    'sexo': 'M',
+    'salario': '10000.0'
+  };
 
-  pessoa.nome = "zils";
-  pessoa.cpf = "089.124.214-50";
-  pessoa.nascimento = DateTime(1995,9,12);
-  pessoa.idade = 26;
-  pessoa.sexo = Sexo.masculino;
-  //pessoa.salario = 2.500;
+  Pessoa pessoa = Pessoa.fromMap(map);
 
-  print(pessoa.toString);
+print(pessoa);
+
+  // Pessoa pessoa = Programador();
+  //
+  // pessoa.nome = "zils";
+  // pessoa.cpf = "089.124.214-50";
+  // pessoa.nascimento = DateTime(1995,9,12);
+  // pessoa.idade = 26;
+  // pessoa.sexo = Sexo.masculino;
+  // //pessoa.salario = 2.500;
+  //
+  // print(pessoa.toString);
 }
 
